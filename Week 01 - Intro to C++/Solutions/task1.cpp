329321ea916f6eb726e2b4651c7f1a64018ee1fa
Vasil Zahariev <vasilzahariev1707@abv.bg>
2022-10-15 20:25:53 +0300

    fix: task1

--- Week 01 - Intro to C++/Solutions/task1.cpp
+++ Week 01 - Intro to C++/Solutions/task1.cpp
@@ -10,11 +10,13 @@ int main() {
     int diffrence = firstNumber + secondNumber;
     int product = firstNumber * secondNumber;
     int quotient = firstNumber / secondNumber;
+    int remainder = firstNumber % secondNumber;
 
     std::cout << "Sum = " << sum << std::endl;
     std::cout << "Diffrence = " << diffrence << std::endl;
     std::cout << "Product = " << product << std::endl;
     std::cout << "Quotient = " << quotient << std::endl;
+    std::cout << "Remainder = " << remainder << std::endl;
 
     return 0;
 }
